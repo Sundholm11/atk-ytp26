b0984eab215d7a2dfb2f03276e4bddedf9370114
Daniel Sundholm <danieljoonatan.sundholm@gmail.com>
2026-09-05 11:00:49 +0300

    setup project fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,32 @@
 // @ts-check
-import { defineConfig } from 'astro/config'
+import { defineConfig, fontProviders } from 'astro/config'
 import vue from '@astrojs/vue'
 
 // https://astro.build/config
 export default defineConfig({
+  fonts: [
+    {
+      name: "VCR OSD Mono",
+      cssVariable: "--font-vcr",
+      provider: fontProviders.local(),
+      options: {
+        variants: [{
+          src: ['./src/assets/fonts/VCR_OSD_MONO_1.001.ttf'],
+          weight: 'normal',
+          style: 'normal'
+        }]
+      },
+      fallbacks: ["VT323", "ui-monospace", "monospace"],
+      optimizedFallbacks: false
+    },
+    {
+      name: "Space Mono",
+      cssVariable: "--font-space-mono",
+      provider: fontProviders.google(),
+      styles: ["normal"],
+      fallbacks: ["ui-monospace", "monospace"],
+      optimizedFallbacks: false
+    }
+  ],
   integrations: [vue()]
 })
